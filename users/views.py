6aabae61efecfaf760f31f9534710051f1d99d6b
Tony-Omondi <otienotony589@gmail.com>
2024-09-23 21:07:13 +0300

    first commit

---
+++ users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'users/index.html', {})
